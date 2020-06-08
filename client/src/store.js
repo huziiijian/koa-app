5cbfed4a9b70e722ccb5e97667bb460d55157e84
huzijian <772964145@qq.com>
2020-06-08 14:08:03 +0800

    dd

--- client/src/store.js
+++ client/src/store.js
@@ -12,5 +12,6 @@ const store = createStore(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   ));
+  // compose 函数做的事就是把 var a = fn1(fn2(fn3(fn4(x)))) 这种嵌套的调用方式改成 var a = compose(fn1,fn2,fn3,fn4)(x) 
 
 export default store;
